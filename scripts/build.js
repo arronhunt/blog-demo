6330b2a326fd12db0e8de02dea0ccefae44852f6
Arron Hunt <arronjhunt@gmail.com>
2020-09-15 09:26:52 -0700

    preview script

--- scripts/build.js
+++ scripts/build.js
@@ -3,12 +3,14 @@ const showdown = require("showdown");
 const fs = require("fs");
 const path = require("path");
 
+const TEST_FILE_NAME = "First blog post";
+
 console.log("Converting...");
 let html;
 try {
   const converter = new showdown.Converter();
   const text = fs.readFileSync(
-    path.resolve(__dirname, "../test/First blog post.md"),
+    path.resolve(__dirname, `../test/${TEST_FILE_NAME}.md`),
     "UTF-8"
   );
   html = converter.makeHtml(text);
@@ -18,7 +20,15 @@ try {
 }
 
 console.log("Building...");
-// TODO
+try {
+  fs.writeFileSync(
+    path.resolve(__dirname, `../html/${TEST_FILE_NAME}.html`),
+    html
+  );
+  console.log("✅ Success");
+} catch (error) {
+  console.log("ERROR:", error);
+}
 
 console.log("Deploying...");
 // TODO
